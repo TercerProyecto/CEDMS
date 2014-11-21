08b21c0173d8075434b6c39e5e2160b800437d25
Ellioth <Ellioth@172.26.111.205>
2014-11-21 02:38:48 -0600

    clase caminoC, nombre de variables cambiadas

--- src/LOGIC/caminoCorto.java
+++ src/LOGIC/caminoCorto.java
@@ -12,46 +12,51 @@ import java.io.IOException;
  * @author Ellioth
  */
 public class caminoCorto {
-    private Lista matriz;
-    private Lista Comp;
-    private Grafo Prinp;
+    //private Lista matriz;
+    //private Lista CompList;
+    private Grafo Principal;
     
     public caminoCorto() throws IOException{
-        matriz =new Lista();
-        Comp= new Lista();
-        Prinp= new Grafo();
+        //matriz =new Lista();
+        //CompList= new Lista();
+        Principal= new Grafo();
     }
  
-    public void setPesos(Nodo _Vector) throws IOException{
-        Nodo tmp= Comp.getHead();
-        Lista temp= new Lista();
-        for (int i=0; i<Comp.length(); i++){
-            temp.insertTail(getDistancia(_Vector,tmp));
-            tmp=tmp.getNext();
+    /**
+     * metodo para hacer la matriz del recorrido mas corto para el grafo
+     * @param _Vector
+     * @throws IOException 
+     */
+    public void setPesos(Object _Vector) throws IOException{
+        /*nodee varComp= CompList.getHead();
+        Lista listVector= new Lista();
+        for (int i=0; i<CompList.length(); i++){
+            listVector.insertTail(getDistancia(_Vector,varComp));
+            varComp=varComp.getNext();
         }
-        tmp=Comp.getHead();
-        Nodo tmp2=matriz.getHead();
-        Lista Ltmp= (Lista)tmp2.get_data();
-        for(int i=0; i<Comp.length(); i++){
-            Ltmp.(getDistancia(tmp,_Vector));
-            tmp=tmp.getNext();
-            tmp2=tmp2.getNext();
-            Ltmp= (Lista)tmp2.get_data();
+        varComp=CompList.getHead();
+        nodee variableMatriz2=matriz.getHead();
+        Lista ListMatriz= (Lista)variableMatriz2.get_data();
+        for(int i=0; i<matriz.length(); i++){
+            ListMatriz.insertTail(getDistancia(varComp,_Vector));
+            varComp =varComp.getNext();
+            variableMatriz2 =variableMatriz2.getNext();
+            ListMatriz= (Lista)variableMatriz2.get_data();
         }
-        matriz.;
+        matriz.insertTail(listVector);
+        */
     }
-    
-    public int getDistancia(Nodo pDato1, Nodo pDato2){
+    /**
+     * metodo que devuelve el dato del valor del camino directo entre dos nodos.
+     * @param pDato1
+     * @param pDato2
+     * @return 
+     */
+    public int getDistancia(Object pDato1, Object pDato2){
         int temp=0;
-        if(Prinp.conexion(pDato1,pDato2)>=0)
-            temp=Prinp.conexion(pDato1,pDato2);
-        else
-            temp;
+        //if(Principal.conexion(pDato1,pDato2)>=0)
+           // temp=Principal.conexion(pDato1,pDato2);
         return temp;
     }
     
-    public static void main (String []args) throws IOException {
-        Lista list= new Lista();
-        list.
-    }
 }
